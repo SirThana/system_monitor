2148a6e4ef461b03b5eedcd324025cb73a3552d4
SirThana <martijn.zijl.3000@gmail.com>
2019-12-15 15:25:25 +0100

    Server now sends a test message to targeted connected clients by their identity (who)

--- backend/client.py
+++ backend/client.py
@@ -1,36 +1,35 @@
 import socket
 import pickle
 import subprocess
+import time
 
 #||||||||||||||||||||||||||||||CLIENT||||||||||||||||||||||||||
 
+
 socket = socket.socket()
-serverAddress = '145.28.187.180', 1111
+serverAddress = 'localhost', 1111
 
 
 def popenExecution(data):
-
-    if data[:2] == 'cd': 
-        os.chdir(str(data[3:]))
-
     command = subprocess.Popen(data, shell=True, stdout=subprocess.PIPE, 
     stderr=subprocess.PIPE, stdin=subprocess.PIPE)
-
     return str(command.stdout.read() + command.stderr.read(), "utf-8")
 
+def recv(socket):
+    try:
+        message = pickle.loads(socket.recv(2048))
+        print(message)
+    except Exception as e:
+        print(e)
 
 def main():
     
+    socket.connect(serverAddress)
+    #Standard command to identify who this machine is
+    socket.send(pickle.dumps(popenExecution("who").split()[0]))
     while True:
-        try:
-            socket.connect(serverAddress)
-            #Standard command to identify who this machine is
-            socket.send(pickle.dumps(popenExecution("who").split()[0]))
-            break
-        except:
-            socket.close()
-            print("Couldn't connect")
-            break
+        recv(socket)
+        time.sleep(1)
 
 
 main()
--- backend/server.py
+++ backend/server.py
@@ -2,12 +2,13 @@ import socket
 import time
 import pickle
 import threading
+import pdb
 
 #||||||||||||||||||||||||||||||SERVER||||||||||||||||||||||||||
 
 
 s = socket.socket()
-serverAddress = ('145.28.187.180', 1111)
+serverAddress = ('localhost', 1111)
 s.bind(serverAddress)
 s.listen(1)
 
@@ -22,14 +23,24 @@ def connHandler(socket, x):
             socketDict.update({pickle.loads(conn.recv(2048)) : conn})
         except Exception as e:
             print(e)
+
+def sendCommands():
+    for key in socketDict.keys():
+        try:
+            x = "test message to : " , key
+            socketDict[key].send(pickle.dumps(str(x)))
+        except:
+            pass
     
 
 
 def main():
     threadedHandler = threading.Thread(target=connHandler, args=(s, 1)) 
     threadedHandler.start()
+    #pdb.set_trace()
     while True:
-        print(socketDict)
+        print(socketDict.keys())
+        sendCommands()
         time.sleep(1)
 
 main()
