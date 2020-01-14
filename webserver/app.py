4f10707cb3afc7688e78a26d2e08af904df5d9d9
ridaak <akazimrida@gmail.com>
2020-01-14 23:03:11 +0100

    fixed columns

--- webserver/app.py
+++ webserver/app.py
@@ -11,22 +11,7 @@ app = Flask(__name__, static_folder="static")
 def index():
     response = requests.get("http://0.0.0.0:8888/&&/")
     data = response.json()
-    machine = []
-    uptime = []
-    user = []
-
-    for who in data:
-        user.append(who)
-        for couple in data[who]:
-            for key, value in couple.items():
-                if 'uname' in key:
-                    machine.append(value)
-                if 'uptime' in key:
-                    uptime.append(value)
-
-    print(machine)
-    print(uptime)
-    return render_template('index.html', user=user, machine=machine, uptime=uptime)
+    return render_template('index.html', data=data)
 
 def main():
     app.run(host="localhost", debug=True)
